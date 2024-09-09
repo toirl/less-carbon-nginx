e7526c265cd41fcb1079844201be64964a44465b
Torsten Irländer <torsten@irlaender.de>
2024-09-09 13:13:19 +0200

    refactor: Use curl instead of ab

--- stress.sh
+++ stress.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 if [ -n "$1" ]; then
-  ab -n 50 -c 5 -H 'Accept-Encoding: gzip' $1
+  for i in {1..100}; do
+    curl -L -H "Accept-Encoding: gzip" $1 >/dev/null
+  done
 fi
+#ab -n 50 -c 5 -H 'Accept-Encoding: gzip' $1
+#curl -L -H "Accept-Encoding: gzip" $1 >/dev/null
