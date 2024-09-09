49d2091ae386f5517b1d3f52c3c84f86e475f879
Torsten Irländer <torsten@irlaender.de>
2024-09-09 21:44:55 +0200

    Actually write file.

--- stress.sh
+++ stress.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -n "$1" ]; then
   for i in $(seq 1 100); do
-    curl -L -S -H "Accept-Encoding: gzip" "$1" >/dev/null
+    curl -O -H "Accept-Encoding: gzip" $1
   done
 fi
 #ab -n 50 -c 5 -H 'Accept-Encoding: gzip' $1
