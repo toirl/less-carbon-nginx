017a5feae257634e0252069232e749dc4047b149
Torsten Irländer <torsten@irlaender.de>
2024-09-09 14:24:30 +0200

    fix: For loop wasn't compatible with sh

--- stress.sh
+++ stress.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -n "$1" ]; then
-  for i in {1..100}; do
-    curl -L -H "Accept-Encoding: gzip" $1 >/dev/null
+  for i in $(seq 1 100); do
+    curl -L -S -H "Accept-Encoding: gzip" "$1" >/dev/null
   done
 fi
 #ab -n 50 -c 5 -H 'Accept-Encoding: gzip' $1
